d6b3450687222da4f12b6c5362cecdb9c85990a3
duncan <duncan.kampert@surf.nl>
2023-03-09 15:44:33 +0100

    Added required environment loading for CMake to allow build

--- env.sh
+++ env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 module load 2022
+module load CMake/3.23.1-GCCcore-11.3.0
 module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
 module load torchvision/0.13.1-foss-2022a-CUDA-11.7.0
 module load matplotlib/3.5.2-foss-2022a
