9abb60c25c38087ff0e6cce453fcf2d9f8c0b399
duncan <duncan.kampert@surf.nl>
2024-10-23 14:00:55 +0200

    Update kernels

--- cxx/kernels.cu
+++ cxx/kernels.cu
@@ -31,17 +31,17 @@ struct dims {
  * It supports any amount of channels, but the output x, y is limited to the maximum thread-block size on the device.
  */
 __global__ void crop_interpolate_kernel(uchar *images, struct dims in_dims, float *transforms,
-                                        float *output_images, struct dims out_dims) {
+                                        uchar *output_images, struct dims out_dims) {
     size_t batch_id = blockIdx.x;
-    size_t x = threadIdx.x;
-    size_t y = threadIdx.y;
+    size_t sub_x = threadIdx.x;
+    size_t sub_y = threadIdx.y;
 
     float *transform = &transforms[batch_id * 4];
     size_t image_size = in_dims.w * in_dims.h * in_dims.c;
     uchar *image = &images[batch_id * image_size];
 
     size_t output_image_size = out_dims.w * out_dims.h * out_dims.c;
-    float *output_image = &output_images[batch_id * output_image_size];
+    uchar *output_image = &output_images[batch_id * output_image_size];
 
     auto avx = float(in_dims.w - out_dims.w);  // Available space X
     auto avy = float(in_dims.h - out_dims.h);  // Available space Y
@@ -56,11 +56,21 @@ __global__ void crop_interpolate_kernel(uchar *images, struct dims in_dims, floa
     auto x_size = min(in_dims.w - x1, (transform[2] * avx) + out_dims.w);
     auto y_size = min(in_dims.h - y1, (transform[3] * avy) + out_dims.h);
 
-    int sx = int(x1 + (x_size / out_dims.w) * x);
-    int sy = int(y1 + (y_size / out_dims.h) * y);
+    // Iterate x chunks if the output image size is larger than allowed block size.
+    for (uint offset_x = 0; offset_x < out_dims.w; offset_x += blockDim.x) {
+        int x = sub_x + offset_x;
+        if (x >= out_dims.w) return;
 
-    for (long c = 0; c < out_dims.c; c++) {
-        output_image[(c * out_dims.w + x) * out_dims.h + y] = float(image[(c * in_dims.w + sx) * in_dims.h + sy]) / 255;
+        int sx = int(x1 + (x_size / out_dims.w) * x);
+        for (uint offset_y = 0; offset_y < out_dims.h; offset_y += blockDim.y) {
+            int y = sub_y + offset_y;
+            if (y >= out_dims.h) return;
+
+            int sy = int(y1 + (y_size / out_dims.h) * y);
+            for (long c = 0; c < out_dims.c; c++) {
+                output_image[(c * out_dims.w + y) * out_dims.h + x] = image[(c * in_dims.w + sy) * in_dims.h + sx];
+            }
+        }
     }
 }
 
@@ -103,7 +113,7 @@ torch::Tensor call_ci_kernel(const torch::Tensor &images,
             long(output_dims.w), long(output_dims.h)
     };
     torch::IntArrayRef out_dims_torch(vec);
-    auto options = torch::TensorOptions().dtype(torch::kFloat32).device(images.device());
+    auto options = torch::TensorOptions().dtype(torch::kUInt8).device(images.device());
     auto output_images = torch::zeros(out_dims_torch, options);
 
     // Wait for memory to finish copying
@@ -112,13 +122,19 @@ torch::Tensor call_ci_kernel(const torch::Tensor &images,
     /*
      * Process kernel
      */
-    dim3 blocks = {uint(output_dims.w), uint(output_dims.h)};
+    dim3 blocks;
+    if (output_dims.w * output_dims.h < 1024) {
+        blocks = {uint(output_dims.w), uint(output_dims.h)};
+    } else {
+        blocks = {32, 32};
+    }
     dim3 grids = {uint(input_dims.b)};
+
     crop_interpolate_kernel<<<grids, blocks>>>(
             images.data_ptr<uchar>(),        // Pass the pointer to the image tensor's data.
             input_dims,                      // Pass the dimensions of the input images
             transforms.data_ptr<float>(),    // Pass the transformation tensor's data
-            output_images.data_ptr<float>(), // Output images tensor's data pointer
+            output_images.data_ptr<uchar>(), // Output images tensor's data pointer
             output_dims                      // Output image tensor dimensions
     );
     // Wait for the CUDA device to finish processing the kernel
@@ -128,5 +144,5 @@ torch::Tensor call_ci_kernel(const torch::Tensor &images,
 }
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-   m.def("crop_interpolate", &call_ci_kernel, "Crop Interpolate");
+    m.def("crop_interpolate", &call_ci_kernel, "Crop Interpolate");
 }
